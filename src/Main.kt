c2c3b60f368e2ac0c4f7e59b1481a3f37eb8d6cf
zhangjia517 <zhangjia517@hotmail.com>
2017-06-16 16:09:13 +0800

    Init

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+/**
+ * Created by Jia on 2017/6/16.
+ * Copyright © 2014-2064 Fostudio. All rights reserved.
+ * Feedback : zhangjia517@hotmail.com
+ * Motto : Harmony in a family is prosperous.
+ */
+fun main(args: Array<String>) {
+    println(here("111"))
+}
---
+++ src/Temp.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by Jia on 2017/6/16.
+ * Copyright © 2014-2064 Fostudio. All rights reserved.
+ * Feedback : zhangjia517@hotmail.com
+ * Motto : Harmony in a family is prosperous.
+ */
+
+fun here(a: String): String
+{
+    var i: Int = 10
+    if (i < 6) {
+        i = 1
+    } else
+    {
+        i = 0
+    }
+    return(a +"wth" + i)
+}
